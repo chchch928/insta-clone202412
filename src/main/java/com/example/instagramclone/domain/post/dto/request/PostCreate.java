3a27569874ad72573eb42fcf4eff3e6c428fb70f
chchch928 <kbanda@naver.com>
2025-01-03 10:52:24 +0900

    #0103-01 [BE/feat] upload image file to local server

--- src/main/java/com/example/instagramclone/domain/post/dto/request/PostCreate.java
+++ src/main/java/com/example/instagramclone/domain/post/dto/request/PostCreate.java
@@ -1,8 +1,12 @@
 package com.example.instagramclone.domain.post.dto.request;
 
+import com.example.instagramclone.domain.post.entity.Post;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 // 필드 생성시 클라이언트가 보낸 JSON 데이터를 파싱하고 검증
 @Getter @Setter @ToString
@@ -16,4 +20,15 @@ public class PostCreate {
 
     @Size(max = 2200, message = "피드 내용은 최대 2200자까지 입력 가능합니다.")
     private String content;
+
+    // 이미지 목록
+    private List<MultipartFile> images;
+
+    // 엔터티 변환 편의메서드
+    public Post toEntity(){
+        return Post.builder()
+                .writer(this.writer)
+                .content(this.content)
+                .build();
+    }
 }
