2e9dd73ede6d8f9748526323bdf887f4e58c1bf6
chchch928 <kbanda@naver.com>
2025-01-14 10:33:34 +0900

    #0114-01 [BE/feat] get current logged in user info API

---
+++ src/main/java/com/example/instagramclone/domain/member/dto/response/MeResponse.java
@@ -0,0 +1,24 @@
+package com.example.instagramclone.domain.member.dto.response;
+
+
+import com.example.instagramclone.domain.member.entity.Member;
+import lombok.Builder;
+import lombok.Getter;
+
+// 인덱스 페이지의 우측상단 suggestion쪽 사용자 정보 렌더링 JSON
+@Getter
+@Builder
+public class MeResponse {
+    private String username;
+    private String name;
+    private String profileImageUrl;
+
+    public static MeResponse from(Member member) {
+        return MeResponse.builder()
+                .username(member.getUsername())
+                .name(member.getName())
+                .profileImageUrl(member.getProfileImageUrl())
+                .build();
+
+    }
+}
