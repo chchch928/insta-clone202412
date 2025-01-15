b190c82f51e0a078c7f83205705a81fab2d7325b
chchch928 <kbanda@naver.com>
2025-01-15 11:36:33 +0900

    #0115-02 [BE/feat] make API for profile page header data

---
+++ src/main/java/com/example/instagramclone/domain/member/dto/response/ProfileHeaderResponse.java
@@ -0,0 +1,30 @@
+package com.example.instagramclone.domain.member.dto.response;
+
+/*
+    프로필 페이지 상단에 렌더링할 데이터들을 JSON으로 변환하는 클래스
+    (프로필 사진, 사용자 이름, 이름, 피드 게시물 수)
+* */
+
+import com.example.instagramclone.domain.member.entity.Member;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ProfileHeaderResponse {
+
+    private String profileImageUrl;
+    private String username;
+    private String name;
+    private long feedCount;
+
+    // 정적 팩토리 메서드
+    public static ProfileHeaderResponse of(Member member, long feedCount) {
+        return ProfileHeaderResponse.builder()
+                .profileImageUrl(member.getProfileImageUrl())
+                .username(member.getUsername())
+                .name(member.getName())
+                .feedCount(feedCount)
+                .build();
+    }
+}
